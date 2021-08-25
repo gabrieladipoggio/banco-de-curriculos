f82371b50fbcc197bc4971400ba49a831f0563f4
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-08-25 20:06:46 -0300

    Adiciona loading button

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
 document.forms['cv_form'].addEventListener('submit', (event) => {
     event.preventDefault();
-    // TODO mostrar que o formulário está sendo enviado
+    const button = document.getElementById('submit-btn');
+    const i = document.querySelector('i');
+    button.classList.add('buttonload');
+    i.classList.add('fa');
+    i.classList.add('fa-circle-o-notch');
+    i.classList.add('fa-spin');
+    
     fetch(event.target.action, {
         method: 'POST',
         body: new URLSearchParams(new FormData(event.target)) // event.target é o form
     }).then((resp) => {
-        console.log(resp)
         if (resp.redirected) {
             window.location.href = resp.url;
         }
@@ -17,6 +22,10 @@ document.forms['cv_form'].addEventListener('submit', (event) => {
     }).catch((error) => {
         // TODO handle error
     });
+    button.classList.remove('buttonload');
+    i.classList.remove('fa');
+    i.classList.remove('fa-circle-o-notch');
+    i.classList.remove('fa-spin');
 });
 
 // Validação CPF 
